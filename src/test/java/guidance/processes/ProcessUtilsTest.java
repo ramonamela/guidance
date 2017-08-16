f33be16e6e98924bb3fad860d189ef03f6e0b870
cramonco <cramonco@e9cd07b6-f61c-4518-8bde-cd466bdb5127>
2017-08-16 12:19:47 +0000

    Add jUnit test for binary execution
    git-svn-id: http://compss.bsc.es/svn/projects/guidance/trunk@1598 e9cd07b6-f61c-4518-8bde-cd466bdb5127

--- src/test/java/guidance/processes/ProcessUtilsTest.java
+++ src/test/java/guidance/processes/ProcessUtilsTest.java
@@ -29,9 +29,14 @@
  *
  */
 
-package guidance;
+package guidance.processes;
 
 import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -39,57 +44,61 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import guidance.processes.ProcessUtils;
 
-public class GuidanceTest {
+
+public class ProcessUtilsTest {
+
+    private static final String TEST_OUTPUT_FILE_1 = "test1.out";
+    private static final String TEST_ERROR_FILE_1 = "test1.err";
+    private static final String[] TEST_FILES = new String[] { TEST_OUTPUT_FILE_1, TEST_ERROR_FILE_1 };
+
 
     @BeforeClass
     public static void runBeforeClass() {
-        // Nothing to setup
+        deleteTestFiles();
     }
 
     @AfterClass
     public static void runAfterClass() {
-        // Nothing to setup
+        deleteTestFiles();
     }
 
     @Before
     public void setUp() {
-        // Nothing to setup
+        deleteTestFiles();
     }
 
     @After
     public void tearDown() {
-        // Nothing to setup
+        deleteTestFiles();
     }
 
-    @Test
-    public void partitionFilePath() {
-        String filePath = "/a/full/path/";
-        String fileName = "fileName";
-        String fileExtension = ".gz";
-
-        String fullFile = filePath + fileName + fileExtension;
-
-        String genFileName = fullFile.substring(0, fullFile.length() - 3);
-        String genFileExt = fullFile.substring(fullFile.length() - 3);
-
-        assertEquals(filePath + fileName, genFileName);
-        assertEquals(fileExtension, genFileExt);
-    }
-
-    @Test
-    public void partitionHeader() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("field1");
-        sb.append("\t");
-        sb.append("field1");
-        for (String field : sb.toString().split("\t")) {
-            assertEquals("field1", field);
+    private static void deleteTestFiles() {
+        for (String fileName : TEST_FILES) {
+            File file = new File(fileName);
+            if (file.exists()) {
+                file.delete();
+            }
         }
+    }
 
-        String header = "field1\tfield1";
-        for (String field : header.split("\t")) {
-            assertEquals("field1", field);
+    @Test
+    public void binaryExitValue() throws IOException {
+        String cmd = "ls";
+        int exitValue = ProcessUtils.execute(cmd, TEST_OUTPUT_FILE_1, TEST_ERROR_FILE_1);
+        assertEquals(0, exitValue);
+    }
+
+    @Test
+    public void binaryOutput() throws IOException {
+        String cmd = "echo hello";
+        int exitValue = ProcessUtils.execute(cmd, TEST_OUTPUT_FILE_1, TEST_ERROR_FILE_1);
+
+        assertEquals(0, exitValue);
+        try (BufferedReader br = new BufferedReader(new FileReader(TEST_OUTPUT_FILE_1))) {
+            String line = br.readLine();
+            assertEquals("hello", line);
         }
     }
 
