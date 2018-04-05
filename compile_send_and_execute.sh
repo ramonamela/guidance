bdbfedb122ebfe0c4008870b1a2b1b017e8e325f
ramonamela <ramon.amela@gmail.com>
2018-04-05 18:11:09 +0200

    COMPSs version in pom.xml and scripts to compile and deploy automatically

---
+++ compile_send_and_execute.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+mvn clean package
+scp guidance.jar pr1ees14@mn1.bsc.es:/gpfs/scratch/pr1ees00/pr1ees14/GCAT/SHAPEIT_IMPUTE
+ssh pr1ees14@mn1.bsc.es 'sbatch /gpfs/scratch/pr1ees00/pr1ees14/GCAT/SHAPEIT_IMPUTE/launch_guidance_sub1000.batch'
