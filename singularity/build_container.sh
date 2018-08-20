563a3d22cffd3db79ea08f19b413ccd5a687a222
ramonamela <ramon.amela@gmail.com>
2018-08-20 11:22:56 +0200

    Samtools and bcftools installed in docker image

--- singularity/build_container.sh
+++ singularity/build_container.sh
@@ -11,10 +11,18 @@ rm -f singularity.img
 
 ## DOCKER IMAGE GENERATION ##
 
+sudo docker ps -a | tail -n +2 | awk '{ print $1 }' | xargs -i sudo docker stop {}
+sudo docker ps -a | tail -n +2 | awk '{ print $1 }' | xargs -i sudo docker rm {}
+
+sudo docker image ls | grep "docker_guidance" | awk '{ print $3 }' | tail -n +2 | xargs -i sudo docker rmi -f {}
+
 pushd docker
 ./build_docker.sh
 popd
 
+#sudo docker stop registry
+#sudo docker ps -a | grep registry:2 | xargs -i sudo docker rm {}
+sudo docker rmi -f registry:2
 sudo docker run -d -p 5000:5000 --restart=always --name registry registry:2
 sudo docker tag docker_guidance localhost:5000/docker_guidance
 sudo docker push localhost:5000/docker_guidance
