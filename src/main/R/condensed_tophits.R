79a5889d9e98d021b27b01ec5aa1bc34f05f939a
mguindom <marta.guindomartinez@bsc.es>
2018-08-22 15:37:30 +0200

    Condensed_top hits fixed to work with only chr 23

--- src/main/R/condensed_tophits.R
+++ src/main/R/condensed_tophits.R
@@ -9,43 +9,49 @@ pval_threshold <- args[6]
 
 pval <- as.numeric(pval_threshold)
 
-if (filtered==filtered_males){
+	if (filtered==filtered_males){
 
-	filtered <- read.table(filtered,header=T,stringsAsFactors=F,na.strings="-")
-	chr <- filtered$chr[1]
+		filtered <- read.table(filtered,header=T,stringsAsFactors=F,na.strings="-")
+		chr <- 23
 
-	if (chr!=23){
-	condensedA <- filtered[,c("chr",
-				"position",
-				"rs_id_all",
-				"info_all",
-				"alleleA",
-				"alleleB",
-				"all_maf",
-				"frequentist_add_se_1",
-				"frequentist_add_beta_1",
-				"frequentist_add_pvalue")]
+        if (nrow(filtered)!=0){
+			chr <- filtered$chr[1]
+		}
 
-	names(condensedA)[c(8,9)] <- c("frequentist_add_se","frequentist_add_beta")
-	condensed <- condensedA
+		if (chr!=23){
+			condensedA <- filtered[,c("chr",
+						"position",
+						"rs_id_all",
+						"info_all",
+						"alleleA",
+						"alleleB",
+						"all_maf",
+						"frequentist_add_se_1",
+						"frequentist_add_beta_1",
+						"frequentist_add_pvalue")]
 
-	} else {
+			names(condensedA)[c(8,9)] <- c("frequentist_add_se","frequentist_add_beta")
+			condensed <- condensedA
 
+		} else {
 	        filtered_males <- read.table(filtered_males,header=T,stringsAsFactors=F,na.strings="-")
-		filtered_females <- read.table(filtered_females,header=T,stringsAsFactors=F,na.strings="-")
+			filtered_females <- read.table(filtered_females,header=T,stringsAsFactors=F,na.strings="-")
 		
 	        condensed_males <- filtered_males[,c("chr",
         	                                "position",
                 	                        "rs_id_all",
                         	                "info_all",
                                 	        "alleleA",
-						"alleleB",
+											"alleleB",
         	                                "all_maf",
                 	                        "frequentist_add_se_1.genotype.sex.1",
                         	                "frequentist_add_beta_1.genotype.sex.1",
                                 	        "frequentist_add_pvalue")]
 
-	        condensed_males$chr <- as.character("23_males")
+			if (nrow(filtered_males)!=0){
+		        	condensed_males$chr <- as.character("23_males")
+			}
+
 	        names(condensed_males)[c(8,9)] <- c("frequentist_add_se","frequentist_add_beta")
 
 	        condensed_females <- filtered_females[,c("chr",
@@ -59,67 +65,71 @@ if (filtered==filtered_males){
                                         	        "frequentist_add_beta_1.genotype.sex.2",
                                                 	"frequentist_add_pvalue")]
 
-	        condensed_females$chr <- as.character("23_females")
+			if (nrow(filtered_females)!=0){
+		        condensed_females$chr <- as.character("23_females")
+			}
+
        		names(condensed_females)[c(8,9)] <- c("frequentist_add_se","frequentist_add_beta")
 
-		names(condensed_females)[c(8,9)] <- c("frequentist_add_se","frequentist_add_beta")
-		condensed <- rbind(condensed_males,condensed_females)
+			condensed <- rbind(condensed_males,condensed_females)
 
 	}
 
 	
-} else {
+	} else {
 
-	filtered <- read.table(filtered,header=T,stringsAsFactors=F,na.strings="-")
-	filtered_males <- read.table(filtered_males,header=T,stringsAsFactors=F,na.strings="-")
-	filtered_females <- read.table(filtered_females,header=T,stringsAsFactors=F,na.strings="-")
+		filtered <- read.table(filtered,header=T,stringsAsFactors=F,na.strings="-")
+		filtered_males <- read.table(filtered_males,header=T,stringsAsFactors=F,na.strings="-")
+		filtered_females <- read.table(filtered_females,header=T,stringsAsFactors=F,na.strings="-")
 
-	condensed <- filtered[,c("chr",
-				"position",
-				"rs_id_all",
-				"info_all",
-				"alleleA",
-				"alleleB",
-				"all_maf",
-				"frequentist_add_se_1",
-				"frequentist_add_beta_1",
-				"frequentist_add_pvalue")]
-
-	condensed$info_all <- as.numeric(condensed$info_all)
-	names(condensed)[c(8,9)] <- c("frequentist_add_se","frequentist_add_beta")
-
-	condensed_males <- filtered_males[,c("chr",
+		condensed <- filtered[,c("chr",
 					"position",
 					"rs_id_all",
-                         		"info_all",
-                   	   		"alleleA",
-                       	    		"alleleB",
-                      			"all_maf",
-                           		"frequentist_add_se_1.genotype.sex.1",
-                          		"frequentist_add_beta_1.genotype.sex.1",
-              				"frequentist_add_pvalue")]
+					"info_all",
+					"alleleA",
+					"alleleB",
+					"all_maf",
+					"frequentist_add_se_1",
+					"frequentist_add_beta_1",
+					"frequentist_add_pvalue")]
 
-	condensed_males$chr <- as.character("23_males")
-	names(condensed_males)[c(8,9)] <- c("frequentist_add_se","frequentist_add_beta")
+		condensed$info_all <- as.numeric(condensed$info_all)
+		names(condensed)[c(8,9)] <- c("frequentist_add_se","frequentist_add_beta")
 
-	condensed_females <- filtered_females[,c("chr",
-        	                		"position",
-   				    	        "rs_id_all",
-               				        "info_all",
-						"alleleA",
-                            			"alleleB",
-                          			"all_maf",
-                        	 		"frequentist_add_se_1.genotype.sex.2",
-                           			"frequentist_add_beta_1.genotype.sex.2",
-               					"frequentist_add_pvalue")]
+		condensed_males <- filtered_males[,c("chr",
+											"position",
+											"rs_id_all",
+			                         		"info_all",
+           				        	   		"alleleA",
+                      		 	    		"alleleB",
+                      						"all_maf",
+                        			   		"frequentist_add_se_1.genotype.sex.1",
+                			          		"frequentist_add_beta_1.genotype.sex.1",
+           					   				"frequentist_add_pvalue")]
 
-	condensed_females$chr <- as.character("23_females")
-	names(condensed_females)[c(8,9)] <- c("frequentist_add_se","frequentist_add_beta")
+		if (nrow(filtered_males)!=0){
+			condensed_males$chr <- as.character("23_males")
+		}
+		names(condensed_males)[c(8,9)] <- c("frequentist_add_se","frequentist_add_beta")
 
+		condensed_females <- filtered_females[,c("chr",
+			        	                		"position",
+				   				    	        "rs_id_all",
+           			    				        "info_all",
+												"alleleA",
+                      			      			"alleleB",
+               				           			"all_maf",
+                        				 		"frequentist_add_se_1.genotype.sex.2",
+                      			     			"frequentist_add_beta_1.genotype.sex.2",
+               									"frequentist_add_pvalue")]
+		if (nrow(filtered_females)!=0){
+			condensed_females$chr <- as.character("23_females")
+		}
+		names(condensed_females)[c(8,9)] <- c("frequentist_add_se","frequentist_add_beta")
 
-	condensed <- rbind(condensed,condensed_males,condensed_females)
-
-}
+		condensed <- rbind(condensed,condensed_males,condensed_females)
+		condensed <- condensed[order(condensed$chr),]
+	}
 
 tophits <- condensed[condensed$frequentist_add_pvalue<=pval,]
 
