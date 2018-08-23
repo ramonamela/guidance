adb4dbcda6e629b93d757224acf6d59b52cb6127
ramonamela <ramon.amela@gmail.com>
2018-08-23 16:06:05 +0200

    Last qqplot_manhattan.R

--- src/main/R/qqplot_manhattan.R
+++ src/main/R/qqplot_manhattan.R
@@ -29,15 +29,15 @@ qq.plot <- function(tab,lambda=T,stat,BA=F,plot=T,mod,
                     pch.col="red",max.yaxis=10,max.xaxis=10,scale.cex=T,scale.fact=0.25,dens=T,...) {
 
     #Calculating LAMBDA
-    chi_list <- qchisq(na.omit(tab$pvalue),df=1,lower.tail=F);
+    chi_list <- qchisq(na.omit(tab$frequentist_add_pvalue),df=1,lower.tail=F);
     l.fac <- median(chi_list)/0.456;
     print(paste(c("LAMBDA = ",l.fac),collapse=""));
 
     #Setting format for plot
-    pvals <- -log10((na.omit(tab$pvalue)));
+    pvals <- -log10((na.omit(tab$frequentist_add_pvalue)));
     tab$col <- "ivory3"
-    tab$col[tab$pvalue <= 5e-8] <- "deeppink"
-    col <- tab$col[!is.na(tab$pvalue)]
+    tab$col[tab$frequentist_add_pvalue <= 5e-8] <- "deeppink"
+    col <- tab$col[!is.na(tab$frequentist_add_pvalue)]
     col <- col[order(pvals)]
     pvals <- pvals[order(pvals)]
     tab$log10 <- pvals
@@ -101,6 +101,7 @@ manhattan <- function(dataframe, colors=c("lightsteelblue4","lightyellow2"),
    d=dataframe
    #Validate input files
    if (!("chr" %in% names(d) & "position" %in% names(d) & "pvalue" %in% names(d))) stop("Make sure your data frame contains columns chr, position, and pvalue")
+
    if (any(limitchromosomes)) d=d[d$chr %in% limitchromosomes, ]
    d=subset(na.omit(d[order(d$chr, d$position), ]), (pvalue>0 & pvalue<=1)) # remove na's, sort, and keep only 0<pvalue<=1
    d$logp = -log10(d$pvalue)
@@ -108,6 +109,7 @@ manhattan <- function(dataframe, colors=c("lightsteelblue4","lightyellow2"),
    ticks=NULL
    lastbase=0
    colors <- c(rep(colors,24)[1:24],"lightsteelblue4")
+
    if (ymax=="max") {ymax<-ceiling(max(d$logp))}
    	ymax<-as.numeric(ymax)
         if (ymax < 8) {ymax<-8}
@@ -132,6 +134,17 @@ manhattan <- function(dataframe, colors=c("lightsteelblue4","lightyellow2"),
                         ticks=c(ticks, d[d$chr==i, ]$pos[floor(length(d[d$chr==i, ]$pos)/2)+1])
                 }
         }
+
+		chr <- unique(d$chr)
+		if (23 %in% chr){ 	
+			ticks[(length(ticks))]
+			ticks[(length(ticks)-1)]
+			ticks2 <- c(ticks[(length(ticks))],ticks[(length(ticks)-1)])
+
+		    d$chr[d$chr=="23"] <- as.character("F")		
+		    d$chr[d$chr=="24"] <- as.character("M")
+		}
+
         if (numchroms==1) {
                 plot(d$pos, d$logp, ylim=c(ymin,ymax), ylab=expression(-log[10](italic(p))),
                         xlab=paste("Chromosome",unique(d$chr),"position"))
@@ -139,6 +152,12 @@ manhattan <- function(dataframe, colors=c("lightsteelblue4","lightyellow2"),
                 plot(d$pos,d$logp, ylim=c(ymin,ymax), ylab=expression(-log[10](italic(p))),
                         xlab="Chromosome", xaxt="n", type="n")
                 axis(1, at=ticks, lab=unique(d$chr))
+
+				if (23 %in% chr){ 
+                	axis(1, at=ticks2, line=2.5,tick=T,lab=rep("",2),lwd.ticks=0)
+					axis(1, at=(as.numeric(ticks2[1])+as.numeric(ticks2[2]))/2, lab=c("X"),line=2,tick=F)
+				}
+
                 icol=1
 
                 for (i in unique(d$chr)) {
@@ -169,26 +188,25 @@ library(gap)
 library(sfsmisc)
 
 
-args <- commandArgs(TRUE)
+	args <- commandArgs(TRUE)
 
-tab_file <- args[1] #tab file with chr, position , pvalue
-out_qqplot <- args[2] #name out qqplot qqplot_namestudy.pdf
-out_manhattan <- args[3] #name out manhattan manhattan_namestudy.pdf
+	tab_file <- args[1] #tab file with chr, position , pvalue
+	out_qqplot <- args[2] #name out qqplot qqplot_namestudy.pdf
+	out_manhattan <- args[3] #name out manhattan manhattan_namestudy.pdf
 
-out_qqplot_tiff <- args[4] #Name output file of qqplot on tiff.
-out_manhattan_tiff <- args[5] #name out manhattan manhattan_namestudy.pdf
-
-out_corrected_pvals <- args[6]  #Name of output file with genomic control corrected pvalues
-
+	out_qqplot_tiff <- args[4] #Name output file of qqplot on tiff.
+	out_manhattan_tiff <- args[5] #name out manhattan manhattan_namestudy.pdf
 
     tab_file_data<-read.delim(tab_file)
-    p <- tab_file_data$pvalue
+    tab_file_data <- tab_file_data[tab_file_data$frequentist_add_pvalue!=0,]
+
+    p <- tab_file_data$frequentist_add_pvalue
     p <- p[!is.na(p)]
     n <- length(p)
     x2obs <- qchisq(p, 1, lower.tail = FALSE)
     x2exp <- qchisq(1:n/n, 1, lower.tail = FALSE)
 
-    #makeqqplot
+#   makeqqplot
     pdf(out_qqplot)
         qq.plot(tab_file_data, lambda=F, stat="pvalue" ,scale.cex=T);
 
@@ -199,13 +217,20 @@ out_corrected_pvals <- args[6]  #Name of output file with genomic control correc
     cat("Q-Q Plot Assoc successfully completed!\n")
     
     #make Manhattan on pdf
+
+    tab_file_data$chr <- as.character(tab_file_data$chr)
+    tab_file_data$chr[tab_file_data$chr=="23_females"] <- as.character("23")
+    tab_file_data$chr[tab_file_data$chr=="23_males"] <- as.character("24")
+    tab_file_data$chr <- as.numeric(tab_file_data$chr)
+
     tab_file_data$SNP <- paste(paste("chr",tab_file_data$chr,sep=""),tab_file_data$position,sep=":")
     tab_man <- data.frame(SNP=tab_file_data$SNP,
                           chr=tab_file_data$chr,
                           position=tab_file_data$position,
-                          pvalue=tab_file_data$pvalue)
+                          pvalue=tab_file_data$frequentist_add_pvalue)
 
     tab_man <- tab_man[tab_man$pvalue <= 0.05,]
+	dim(tab_man)
 
     title <- "Manhattan-plot"
     YMIN <- 0
@@ -220,7 +245,6 @@ out_corrected_pvals <- args[6]  #Name of output file with genomic control correc
         manhattan(tab_man, pch=16,cex=0.70,main=title,colors=c("lightsteelblue4","ivory3"), suggestiveline=-log10(5e-8), ymax=YMAX, ymin=YMIN)
     dev.off()
 
-
-    write.table(tab_man,out_corrected_pvals,row.names=F,sep="\t",quote=F)
+#    write.table(tab_man,out_corrected_pvals,row.names=F,sep="\t",quote=F)
 
     cat("MANHATTAN PLOT successfully completed!\n")
