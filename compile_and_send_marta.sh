7d08b0a2c66fd7dadac4a8435f1ff4c741a5f576
mguindom <marta.guindomartinez@bsc.es>
2019-01-25 11:11:17 +0100

    spaces

--- compile_and_send_marta.sh
+++ compile_and_send_marta.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 mvn clean package
-scp {guidance.jar,set_environment.sh,set_environment_original.sh,set_environment_separate.sh,set_environment_separate_constant.sh,set_environment_variable.sh} pr1ees03@mn1.bsc.es:/gpfs/scratch/pr1ees00/pr1ees03/GERA/barriers/
+scp guidance.jar bsc05997@mn1.bsc.es:/gpfs/projects/bsc05/martagm/GWImp_COMPSs/GERA_sub200/
